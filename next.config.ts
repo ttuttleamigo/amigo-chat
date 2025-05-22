7315abb0b943db1f13bd6eb23abdf493c426561b
Trent Tuttle <ttuttle@myamigo.com>
2025-05-22 15:00:32 -0400

    Configure basePath and unoptimized images for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/amigo-chat',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
