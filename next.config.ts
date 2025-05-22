d06ab01bf91537886a294cd50e3f819f432ea9fc
Trent Tuttle <ttuttle@myamigo.com>
2025-05-22 15:03:26 -0400

    Add assetPrefix and trailingSlash for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/amigo-chat',
+  assetPrefix: '/amigo-chat',
+  trailingSlash: true,
   images: {
     unoptimized: true,
     remotePatterns: [
